4cd661e4ff630abd8b4d89c5ecb99bfb140a3767
drlxj <xuejingluo98@gmail.com>
2022-05-16 13:34:35 +0200

    [modified] net add rank

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ import random
 import time
 
 def setup(rank, world_size):
-    os.enviro['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_ADDR'] = 'localhost'
     os.environ['MASTER_PORT'] = '12355'
     # initialize the process group
     dist.init_process_group("gloo", rank=rank, world_size=world_size)
@@ -99,7 +99,7 @@ if __name__ == '__main__':
 
     random.seed(time.time())
 
-    mp.spwan(train_basic,
+    mp.spawn(train_basic,
             args=(world_size, cfg),
             nprocs=world_size,
             join=True)
